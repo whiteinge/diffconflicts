b10cd62e433eaaf29e947a38d562984b53e162a9
Harald Klimach <harald.klimach@uni-siegen.de>
2019-07-02 11:14:11 +0200

    Changed ordering of changesets in make-conflicts_hg.sh to actually match make-conflicts.sh.

--- _utils/make-conflicts_hg.sh
+++ _utils/make-conflicts_hg.sh
@@ -20,23 +20,6 @@ hg bookmark master
 hg commit -m 'Commit One'
 
 cat << EOF > poem.txt
-twas brillig, and the slithy toves
-Did gyre and gimble in the wabe:
-all mimsy were the borogoves,
-And the mome raths outgrabe.
-
-"Beware the Jabberwock, my son!
-The jaws that bite, the claws that catch!
-Beware the Jubjub bird, and shun
-The frumious Bandersnatch!"
-EOF
-
-hg bookmark branchA
-hg commit -m 'Fix syntax mistakes'
-
-hg update -Cr master
-
-cat << EOF > poem.txt
 'Twas brillig, and the slithy toves
 Did gyre and gimble in the wabe:
 All mimsy were the borogroves
@@ -48,6 +31,22 @@ Beware the Jub jub bird, and shun
 The frumious bandersnatch!"
 EOF
 
+hg bookmark branchA
 hg commit -m 'Buncha fixes'
+
+
+hg update -Cr master
+cat << EOF > poem.txt
+twas brillig, and the slithy toves
+Did gyre and gimble in the wabe:
+all mimsy were the borogoves,
+And the mome raths outgrabe.
+
+"Beware the Jabberwock, my son!
+The jaws that bite, the claws that catch!
+Beware the Jubjub bird, and shun
+The frumious Bandersnatch!"
+EOF
+hg commit -m 'Fix syntax mistakes'
 
 hg --config ui.merge=internal:merge merge branchA
